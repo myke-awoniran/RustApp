325b9389e10bb87e73391023bcbd53bb9ea9a926
myke <awoniranopeyemi@gmail.com>
2023-05-24 14:50:11 +0100

    add fn michael

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
+use std::io;
+
 fn main() {
     println!("Hello, world!");
     michael()
 }
 
+
 fn michael() {
-    println!("michael is coming soon")
+    let a = [1, 2, 3, 4, 5, 6];
+    println!("Pls enter an array index");
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index).expect("failed to read line");
+
+    let index: usize = index.trim().parse().expect("index entered was not a number");
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is : {element}");
 }
