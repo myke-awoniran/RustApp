2facff43983b44a311875bf947cf24402b844337
myke <awoniranopeyemi@gmail.com>
2023-05-28 13:17:26 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -16,26 +16,34 @@ fn michael() {
     let index: usize = index.trim().parse().expect("index entered was not a number");
 
     let element = a[index];
-    let mut x = five();
+    // let mut x = five();
 
-    x = plus_one(x);
-    for_loop();println!("The value of the element at index {index} is : {element}, {x}");
+    // x = plus_one(x);
+    // for_loop();
+    raii();
+    println!("The value of the element at index {index} is : {element}");
 }
 
-fn five() -> i32 {
-    5
+// fn five() -> i32 {
+//     5
+// }
+
+// fn plus_one(x: i32) -> i32 {
+//     if x > 5 {
+//         println!("x value {x}, is not allow on this function call");
+//     }
+//     x + 1
+// }
+
+fn raii() {
+    let first_name = String::from("micheal");
+    let last_name = first_name;
+    println!("firstname is {}", last_name);
 }
 
-fn plus_one(x: i32) -> i32 {
-    if x > 5 {
-        println!("x value {x}, is not allow on this function call");
-    }
-    x + 1
-}
-
-fn for_loop() {
-    const ARRAY: [i32; 6] = [3, 2, 3, 42, 3, 23, ];
-    for a in ARRAY {
-        println!("the value of this array is {a}");
-    }
-}
+// fn for_loop() {
+//     const ARRAY: [i32; 6] = [3, 2, 3, 42, 3, 23, ];
+//     for a in ARRAY {
+//         println!("the value of this array is {a}");
+//     }
+// }
